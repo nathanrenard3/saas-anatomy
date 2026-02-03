b7839a8714705e1b738443f495b6d05ed18f4a5f
Nathan Renard <n.renard38350@gmail.com>
2026-02-03 17:58:05 +0100

    Update GoogleAnalytics component to use a hardcoded GA measurement ID, removing the previous environment variable check.

--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -1,12 +1,5 @@
 import { GoogleAnalytics as NextGoogleAnalytics } from '@next/third-parties/google'
 
 export function GoogleAnalytics() {
-  const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
-
-  if (!gaId) {
-    console.warn('Google Analytics: NEXT_PUBLIC_GA_MEASUREMENT_ID is not defined')
-    return null
-  }
-
-  return <NextGoogleAnalytics gaId={gaId} />
+  return <NextGoogleAnalytics gaId="G-ZXD1ZXR17N" />
 }
